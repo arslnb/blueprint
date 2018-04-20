12eef1843d8d229a26c8512b67131f81eff93a83
Arsalan Bashir <arsalan.b4@gmail.com>
2018-04-20 10:03:45 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Blueprint
+
+Demonstrates how backbone and underscore can combine to avoid heavy DOM manipulation in apps with server-side-rendering. `Server.py` handles routing and also functions as a simple mock-API. The frontend then uses backbone and underscore (and Bootstrap) to consume API. 
+
+This is an exercise on combining SSR apps with the clientside MVC design pattern. 
---
+++ server/server.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+from flask import Blueprint
+
+app = Flask(__name__)
+
+client = Blueprint('client', __name__, 
+     static_folder='../client/static',
+     template_folder='../client/templates')
+
+@client.route('/')
+def home():
+    return render_template('index.html')
+
+app.register_blueprint(client)
+
+@app.route('/collection')
+def collections():
+    return "collection"
+
+@app.route('/model', methods=["GET"])
+@app.route('/model/<id>', methods=["GET", "POST"])
+def models(id):
+    return "model"
+
+if __name__ == "__main__":
+    app.run(debug = True)
